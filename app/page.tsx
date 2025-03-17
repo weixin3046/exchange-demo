f5891df8201dc1a4a1b192a532922b6fb31948a1
Jerry Mi <408516111@qq.com>
2025-03-17 18:18:35 +0800

    feat: 引入shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
     <div>
       <ConnectButton />
+      <Button>Click me</Button>
     </div>
   );
 }
