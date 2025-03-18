b4699de8b294eea1aaf5f1bcd2627c3eebd88f27
Jerry Mi <408516111@qq.com>
2025-03-18 12:26:42 +0800

    feat: 国际化路由跳转

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 import LocaleSwitcher from "@/components/LocaleSwitcher";
 import TradeLayout from "@/components/TradeLayout";
+import { Link } from "@/i18n/navigation";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 
 export default function Home() {
   const t = useTranslations("HomePage");
